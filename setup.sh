11846d70675afb86e4f0581a64a06d1b751a2ce3
JJ5421 <jeremykuz03@gmail.com>
2024-07-12 22:14:31 -0400

    RPI5 GPIO should be fixed

--- setup.sh
+++ setup.sh
@@ -5,16 +5,7 @@ echo "Installing Python and pip..."
 apt-get update
 apt-get install -y python3 python3-pip
 
-# Step 1: Install pigpiod
-echo "Installing pigpiod..."
-cd /tmp
-git clone https://github.com/joan2937/pigpio.git
-cd pigpio
-make
-make install
-pip3 install pigpio
-
-# Step 2: Install Docker
+# Step 1: Install Docker
 echo "Installing Docker..."
 apt-get update
 apt-get install -y \
@@ -30,42 +21,45 @@ echo \
 apt-get update
 apt-get install -y docker-ce docker-ce-cli containerd.io
 
-# Step 3: Set Docker daemon to run on startup
+# Step 2: Set Docker daemon to run on startup
 echo "Setting Docker to run on startup..."
 systemctl enable docker
 
-# Step 4: Install cron
+# Step 3: Install cron
 echo "Installing cron..."
 apt-get install -y cron
 
-# Step 5: Add cron job for @reboot
+# Step 4: Add cron job for @reboot
 systemctl enable cron
 echo "Adding cron job for @reboot..."
 (crontab -l 2>/dev/null; echo "@reboot /home/cdcl/wearable-biosensors-ros2/pi_startup.sh") | crontab -
 
-# Step 6: Allow user cdcl to run shutdown and pigpiod without sudo
+# Step 5: Allow user cdcl to run shutdown without sudo
 echo "Updating sudoers file for user cdcl..."
-echo "cdcl ALL=(ALL) NOPASSWD: /sbin/shutdown, /usr/bin/pigpiod" | sudo tee /etc/sudoers.d/cdcl
+echo "cdcl ALL=(ALL) NOPASSWD: /sbin/shutdown" | sudo tee /etc/sudoers.d/cdcl
 
 usermod -aG docker cdcl
 
-# Step 7: Clone the git repo
+# Step 6: Clone the git repo
 echo "Cloning the git repository..."
 cd /home/cdcl
 git clone https://github.com/JJ5421/wearable-biosensors-ros2.git
 
-# Step 8: Make pi_startup.sh executable
+# Step 7: Make pi_startup.sh executable
 echo "Making pi_startup.sh executable..."
 chmod +x /home/cdcl/wearable-biosensors-ros2/pi_startup.sh
 
-# Step 9: Change permissions for ros_bags
+# Step 8: Change permissions for ros_bags
 echo "Changing permissions for ros_bags..."
 chmod 777 /home/cdcl/wearable-biosensors-ros2/ros_bags
 
-# Step 10: Build the Docker in cdcl-humble-jammy-biosensors
+# Step 9: Install gpiozero
+echo "Installing gpiozero..."
+pip3 install gpiozero
+
+# Step 10: Build the Docker in cdcl-humble-jammy-biosensors (assuming build.sh exists)
 echo "Building the Docker image..."
 cd /home/cdcl/wearable-biosensors-ros2/cdcl-humble-jammy-biosensors
 ./build.sh
 
 echo "Setup complete!"
-
