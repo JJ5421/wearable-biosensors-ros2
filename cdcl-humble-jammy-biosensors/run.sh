fd570328728ac0406376c67f7c84e48598b1c4f1
JJ5421 <jeremykuz03@gmail.com>
2024-07-12 23:57:31 -0400

    test

--- cdcl-humble-jammy-biosensors/run.sh
+++ cdcl-humble-jammy-biosensors/run.sh
@@ -12,6 +12,7 @@ docker run --restart=always \
     -v /home/cdcl/wearable-biosensors-ros2/ros_bags:/home/cdcl/wearable-biosensors-ros2/ros_bags \
     -v /var/run/dbus/system_bus_socket:/var/run/dbus/system_bus_socket \
     -v /dev/mem:/dev/mem \
+    -v /dev/gpiomem:/dev/gpiomem \
     -e DBUS_SESSION_BUS_ADDRESS="/var/run/dbus/system_bus_socket" \
     --device=/dev/hidraw1 \
     --device=/dev/mem \
